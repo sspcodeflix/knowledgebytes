804442752b1e0dd3fe022953807b92be5bdf414e
ashish168527 <17795229+ashish168527@users.noreply.github.com>
2024-06-20 15:03:08 +0000

    nginx & certbot configuration updates

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,6 @@
-import os
-import logging
 from app import create_app
-from flask_cors import CORS
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+app = create_app('production')
 
-if __name__ == '__main__':
-    env = os.getenv('FLASK_ENV', 'development') # Fetch the environment from .env if not found use default 'development'
-    logger.info(f"Starting application in {env.upper()} environment")
-
-    app = create_app(env)
-    cors = CORS(app, resources={r"/api/*": {"origins": "https://superstarbot.online"}})
-
-    app.config['CORS_HEADERS'] = 'Content-Type'
-
-    logger.info("CORS configuration completed")
-    logger.info("Application is running")
-
+if __name__ == "__main__":
     app.run()
